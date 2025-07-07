bd8fe4c8a430592c28d31e3d29e5f18f7ae5cf95
droptonotify <droptonotify@icloud.com>
2025-07-07 17:23:39 +0000

    chore: update env example and add vercel.json for vercel deployment

--- backend/app/infrastructure/config.py
+++ backend/app/infrastructure/config.py
@@ -59,4 +59,49 @@ class Settings(BaseSettings):
 def get_settings() -> Settings:
     settings = Settings()
     settings.validate()
-    return settings 
+    return settings
+
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import base64
+import os
+from google import genai
+from google.genai import types
+
+
+def generate():
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-2.5-pro"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
+            ],
+        ),
+    ]
+    tools = [
+        types.Tool(url_context=types.UrlContext()),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        temperature=0.6,
+        thinking_config = types.ThinkingConfig(
+            thinking_budget=31179,
+        ),
+        tools=tools,
+        response_mime_type="text/plain",
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+if __name__ == "__main__":
+    generate()
